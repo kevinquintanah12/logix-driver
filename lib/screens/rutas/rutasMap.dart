007335c958e35809ba032075fc0879fb3d3d2623
ete12 <emilianotoes12@gmail.com>
2025-03-29 09:34:40 -0600

    Mapa de Logix

--- lib/screens/rutas/rutasMap.dart
+++ lib/screens/rutas/rutasMap.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class RutasMap extends StatefulWidget {
   const RutasMap({Key? key}) : super(key: key);
@@ -8,6 +10,8 @@ class RutasMap extends StatefulWidget {
 }
 
 class _RutasMapState extends State<RutasMap> {
+  final String mapboxToken = "pk.eyJ1IjoiZXRlMTIiLCJhIjoiY204c3luNmw1MDVvdTJscHk2MGNsYXZseSJ9.lsWk079QN6p2c80GZTPonQ"; // Reemplaza con tu token
+
   @override
   Widget build(BuildContext context) {
     const Color primaryBlue = Color(0xFF0B3C5D);
@@ -19,19 +23,33 @@ class _RutasMapState extends State<RutasMap> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Column(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(19.0595, -96.7317), // Coordenadas iniciales (Orizaba)
+          zoom: 13.0,
+        ),
         children: [
-          // Contenedor donde irá el mapa de Mapbox
-          Expanded(
-            child: Container(
-              color: Colors.grey[300], // Temporal para visualizar la zona del mapa
-              child: const Center(
-                child: Text(
-                  'Aquí irá el mapa de Mapbox',
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
+          TileLayer(
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}@2x?access_token=$mapboxToken",
+            additionalOptions: {
+              'accessToken': mapboxToken,
+              'id': 'mapbox.streets',
+            },
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                width: 40.0,
+                height: 40.0,
+                point: LatLng(19.0595, -96.7317), // Punto de referencia
+                builder: (ctx) => const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
                 ),
               ),
-            ),
+            ],
           ),
         ],
       ),
