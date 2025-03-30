3dd6a946e152cfc2b994f77103618a0c773d92cc
JorgeLuisTuzMoreno <tuzmoreno.jorgeluis@gmail.com>
2025-03-29 19:44:37 -0600

    primera propuesta de issue 10

---
+++ lib/query/paquetes_query.dart
@@ -0,0 +1,41 @@
+const String queryPorEntregar = """
+  query {
+    entregasPorEstado(estado: "por entregar") {
+      id
+      paquete {
+        id
+        producto {
+          id
+        }
+      }
+      destinatario {
+        nombre
+        latitud
+        longitud
+      }
+      estado
+      fechaEntrega
+    }
+  }
+""";
+
+const String queryEntregadas = """
+  query {
+    entregasPorEstado(estado: "entregados") {
+      id
+      paquete {
+        id
+        producto {
+          id
+        }
+      }
+      destinatario {
+        nombre
+        latitud
+        longitud
+      }
+      estado
+      fechaEntrega
+    }
+  }
+""";
